dc9d56d12a1104ea9ff8e4fe7fd3799158121318
Cheng Zhao <zcbenz@gmail.com>
2014-10-18 20:50:39 +0800

    Load after onload event

--- chinese-auto-sim-to-trans.js
+++ chinese-auto-sim-to-trans.js
@@ -5,10 +5,12 @@
   var supported = ['zh-tw', 'zh-sg', 'zh-hk', 'zh-hant'];
   if (supported.indexOf(navigator.language) == -1) return;
 
-  var tag = document.createElement('script');
-  tag.type = 'text/javascript';
-  tag.async = true;
-  tag.src = './lazy-loaded.js';
-  (document.getElementsByTagName('head')[0] ||
-   document.getElementsByTagName('body')[0]).appendChild(tag);
+  window.onload = function() {
+    var tag = document.createElement('script');
+    tag.type = 'text/javascript';
+    tag.async = true;
+    tag.src = './lazy-loaded.js';
+    (document.getElementsByTagName('head')[0] ||
+     document.getElementsByTagName('body')[0]).appendChild(tag);
+  };
 })();
