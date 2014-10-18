63371b80b15866d7b1be13d2de1b7db82f9932a4
Cheng Zhao <zcbenz@gmail.com>
2014-10-18 20:35:15 +0800

    Add code

---
+++ chinese-auto-sim-to-trans.js
@@ -0,0 +1,14 @@
+(function() {
+  var isIE = !!(window.attachEvent && !window.opera);
+  if (isIE) return;
+
+  var supported = ['zh-tw', 'zh-sg', 'zh-hk', 'zh-hant'];
+  if (supported.indexOf(navigator.language) == -1) return;
+
+  var tag = document.createElement('script');
+  tag.type = 'text/javascript';
+  tag.async = true;
+  tag.src = './lazy-loaded.js';
+  (document.getElementsByTagName('head')[0] ||
+   document.getElementsByTagName('body')[0]).appendChild(tag);
+})();
